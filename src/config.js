ad39ca77d7c8b9f0b3248a7ee3ebe702345460c9
thondery <thondery@163.com>
2017-02-16 17:43:25 +0800

    修复config.js中找不到production环境的配置文件

--- src/config.js
+++ src/config.js
@@ -1,6 +1,14 @@
+import fs from 'fs'
+import path from 'path'
+
 const ENV_DEVELOPMENT = 'development'
 const ENV_PRODUCTION  = 'production'
 const ENV_TESTING     = 'test'
+
+if (!fs.existsSync(path.resolve(__dirname, 'config.production.js'))) {
+  let confStr = fs.readFileSync(path.resolve(__dirname, 'config.development.js'), 'utf-8')
+  fs.writeFileSync(path.resolve(__dirname, 'config.production.js'), confStr, { encoding: 'utf-8' })
+}
 
 const config = {
   [ENV_PRODUCTION]    : require('./config.production'),
